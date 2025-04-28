3c9c5a818c86a6f6f08a6cfd8f183cf6764fa482
birzhan2030 <62757361+birzhan2030@users.noreply.github.com>
2025-04-28 11:17:41 +0500

    day 2

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,6 +1,21 @@
+import UserCard from "@/components/UserCard"
+
 const Adminpage = () => {
   return (
-    <div> Adminpage </div>
+    <div className="p-4 flex flex-col md:flex-row gap-4">
+      {/*Left*/}
+      <div className="w-full lg:w-2/3">
+      {/*UserCards*/}
+        <div className="flex flex-wrap gap-4 justify-between">
+          <UserCard type="Student"/>
+          <UserCard type="Teacher"/>
+          <UserCard type="Parent"/>
+          <UserCard type="Staff"/>
+        </div>
+      </div>
+      {/*right*/}
+      <div className="w-full lg:w-1/3"></div>  
+    </div>
   )
 }
 
